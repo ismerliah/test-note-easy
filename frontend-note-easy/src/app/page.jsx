4f39c355b86610c595b5f9bf479ec371d21293bf
Yanisa Thaenthong <mook.yanisa@gmail.com>
2024-08-01 00:23:54 +0700

    Create HomePage

--- frontend-note-easy/src/app/page.jsx
+++ frontend-note-easy/src/app/page.jsx
@@ -8,9 +8,31 @@ export const metadata = {
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
-      <Link href="/register">Register</Link>
-      <Link href="/signin">Sign in</Link>
+      <div className="bg-slate-200 flex h-screen flex-1 flex-col justify-center items-center">
+        <div className="bg-white rounded-lg border-y-indigo-950 shadow-lg p-12 md:w-2/5">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 className="mt-8 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900">
+              Note Easy
+            </h2>
+          </div>
+
+          <div className="mt-10 space-y-9 sm:mx-auto sm:w-full sm:max-w-sm">
+            <a
+              href="register"
+              className="flex w-50 justify-center rounded-md bg-gray-900 px-3 py-3 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-slate-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+            >
+              Register
+            </a>
+
+            <a
+              href="signin"
+              className="flex w-full justify-center rounded-md bg-gray-900 px-3 py-3 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-slate-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
+            >
+              Sign in
+            </a>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
