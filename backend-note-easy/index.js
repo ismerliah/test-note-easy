b20cd6f1f9ed4190e4a865c01d5abc1c46d7c09a
Yanisa Thaenthong <mook.yanisa@gmail.com>
2024-08-04 12:30:56 +0700

    fix cors 0.2

--- backend-note-easy/index.js
+++ backend-note-easy/index.js
@@ -12,15 +12,12 @@ const CategoryModel = require('./models/note-category')
 
 const app = express()
 app.use(express.json())
-app.use(cors())
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+const cors = require('cors');
+app.use(cors({
+  origin: 'https://test-note-easy-fe.vercel.app'
+}));
+app.options('*', cors());
 
 app.use(cookieParser())
 
