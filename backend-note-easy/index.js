44a6f5359b3229372cffd2aa6bf5a8d2525a8798
Yanisa Thaenthong <mook.yanisa@gmail.com>
2024-08-04 13:08:07 +0700

    update vercel.json 2

--- backend-note-easy/index.js
+++ backend-note-easy/index.js
@@ -5,10 +5,9 @@ const bycrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
 
-
-const UserModel = require('../models/user')
-const NoteModel = require('../models/note')
-const CategoryModel = require('../models/note-category')
+const UserModel = require('./models/user')
+const NoteModel = require('./models/note')
+const CategoryModel = require('./models/note-category')
 
 const app = express()
 app.use(express.json())
