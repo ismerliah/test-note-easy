2a7ccb19b5656e55258c94eae14e0356f363ac72
Yanisa Thaenthong <mook.yanisa@gmail.com>
2024-08-04 12:04:32 +0700

    fix error

--- frontend-note-easy/src/app/signin/page.jsx
+++ frontend-note-easy/src/app/signin/page.jsx
@@ -90,7 +90,7 @@ export default function SigninPage() {
           </form>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            Don't have an account? {""}
+            Do not have an account?
             <a
               href="register"
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
