4f59bf559c671b5ebf8449c1b439c2dd8288d0b9
Yanisa Thaenthong <mook.yanisa@gmail.com>
2024-08-04 12:19:10 +0700

    fix cors

--- backend-note-easy/index.js
+++ backend-note-easy/index.js
@@ -12,11 +12,7 @@ const CategoryModel = require('./models/note-category')
 
 const app = express()
 app.use(express.json())
-app.use(cors({
-    //origin: ["http://localhost:3000"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}))
+app.use(cors())
 
 app.use(cookieParser())
 
