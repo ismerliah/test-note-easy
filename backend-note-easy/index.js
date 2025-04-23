fffe7b9d082dbf438751615dfc1d0458ca050edd
Yanisa Thaenthong <yanisa.thaenthong@gmail.com>
2025-04-24 03:12:28 +0700

    fix: cors for prod

--- backend-note-easy/index.js
+++ backend-note-easy/index.js
@@ -13,20 +13,22 @@ const CategoryModel = require('./models/note-category')
 const app = express()
 app.use(express.json())
 
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//   })
-// );
-// app.options('*', cors());
-
+// PROD
 app.use(
   cors({
     origin: 'http://localhost:3000',
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
   })
 );
+app.options('*', cors());
+
+// test
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
 
 app.use(cookieParser())
 
